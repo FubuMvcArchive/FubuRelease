d6f1747ea404a048aae66b2fe48aad011efbbe8c
jeremydmiller <jeremydmiller@yahoo.com>
2012-10-05 21:03:35 -0500

    first big push

---
+++ src/FubuRelease.Testing/StandinTester.cs
@@ -0,0 +1,15 @@
+﻿using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuRelease.Testing
+{
+    [TestFixture]
+    public class StandinTester
+    {
+        [Test]
+        public void it_is_all_good()
+        {
+            1.ShouldEqual(1);
+        }
+    }
+}
